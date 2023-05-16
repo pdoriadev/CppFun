a17bdd4e6cf20057dff7dd95df5ecce8e29ef67a
Peter Doria <pdoriadev@gmail.com>
2023-05-16 00:02:19 -0700

    Moved Console manipulation methods to their own header file. Added #include reference to header file in .cpp source file

---
+++ CppFun/RectangleDimensions/RectangleDimension/WinConsoleMethods.h
@@ -0,0 +1,70 @@
+#pragma once
+#include <iostream>
+#include <iomanip>
+#include <windows.h> 
+
+/******************************************************
+	*                    placeCursor                     *
+	******************************************************/
+void placeCursor(HANDLE screen, int row, int col)
+{                       // COORD is a defined C++ structure that
+	COORD position;     // holds a pair of X and Y coordinates
+	position.Y = row;
+	position.X = col;
+	SetConsoleCursorPosition(screen, position);
+}
+
+void shiftCursorFromCurrentPos(HANDLE screen, int rowShift, int colShift)
+{
+	COORD position = getConsoleCursorPosition(screen);
+	position.Y += rowShift;
+	position.Y += colShift;
+	SetConsoleCursorPosition(screen, position);
+}
+
+COORD getConsoleCursorPosition(HANDLE screen)
+{
+	CONSOLE_SCREEN_BUFFER_INFO bufferInfo;
+	if (GetConsoleScreenBufferInfo(screen, &bufferInfo))
+	{
+		return bufferInfo.dwCursorPosition;
+	}
+	else
+	{
+		COORD invalid = { 0, 0 };
+		return invalid;
+	}
+}
+
+_SMALL_RECT getConsoleRect(HANDLE screen)
+{
+	CONSOLE_SCREEN_BUFFER_INFO bufferInfo;
+	if (GetConsoleScreenBufferInfo(screen, &bufferInfo))
+	{
+		return bufferInfo.srWindow;
+	}
+	else
+	{
+		_SMALL_RECT invalid = { 0, 0, 0, 0 };
+		return invalid;
+	}
+}
+
+void clearCurrentConsoleLine(HANDLE screen)
+{
+	_SMALL_RECT consoleRect = getConsoleRect(screen);
+	COORD cursorStartPos = getConsoleCursorPosition(screen);
+	placeCursor(screen, cursorStartPos.Y, consoleRect.Left);
+	std::cout << std::right << std::setfill(' ') << std::setw(consoleRect.Right) << '.' << std::flush;
+}
+
+void clearConsole(HANDLE screen)
+{
+	_SMALL_RECT consoleRect = getConsoleRect(screen);
+	placeCursor(screen, consoleRect.Top, consoleRect.Left);
+	for (int i = 0; i < consoleRect.Bottom; i++)
+	{
+		std::cout << std::right << std::setfill(' ') << std::setw(consoleRect.Right) << ' ' << std::flush;
+		placeCursor(screen, consoleRect.Top + i, consoleRect.Left);
+	}
+}
