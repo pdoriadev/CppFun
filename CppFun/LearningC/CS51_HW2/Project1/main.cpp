191e9203fd3bb2a9fa4e908b7802e1c36fcbd0ed
Peter Doria <pdoriadev@gmail.com>
2024-03-07 12:27:30 -0800

    C lang homework for CS51 Computer Architecture. Finished HW2. Started on Lab 2 Part 0.

---
+++ CppFun/LearningC/CS51_HW2/Project1/main.cpp
@@ -0,0 +1,47 @@
+
+
+/* Lab 02 Part 0
+	Submit your Hello World C source code in text. 
+	You can copy your source code in the textbox and submit.
+*/
+
+int main()
+{
+	
+}
+
+
+
+/*	HW2 - write function that returns length of a string. 
+		Use given function:
+			int length(char *p) {}
+		code inside main is also given
+*/
+
+/*
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int length(char* p)
+{
+	int i = 0;
+	while (*p != NULL)
+	{
+		i++;
+		p++;
+	}
+	return i;
+}
+
+int main()
+{
+	char str[20];
+	printf("Input string: ");
+	scanf("%[^\n]%*c", str);
+
+	int len = length(str);
+	printf("The length of string is %d.", len );
+	getchar();
+}
+
+*/
