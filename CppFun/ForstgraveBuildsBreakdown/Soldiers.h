e9aca523abfb1147e901bb7b124a809e30ac6388
Peter Doria <pdoriadev@gmail.com>
2024-05-03 02:02:45 -0700

    Frostgrave Soldiers.h - added SoldierInstance class that can represent a soldier in a given party.

--- CppFun/ForstgraveBuildsBreakdown/Soldiers.h
+++ CppFun/ForstgraveBuildsBreakdown/Soldiers.h
@@ -3,37 +3,76 @@
 #include "Items.h"
 #include <vector>
 #include <string>
+#include <assert.h>
 
-// this is where all the soldier data goes
-class Soldier
+namespace Soldiers
 {
-	std::string name = "";
-	Attributes attributes;
-	std::vector<ItemInterface> items;
-	int maxItems = 1;
-	// weapons
-	// cost
 
-public:
-	Soldier(std::string _name, Attributes _attributes, int _maxItems)
+	// used to represent a specific soldier
+	class SoldierInstance
 	{
-		name = _name;
-		attributes = _attributes;
-		maxItems = _maxItems;
-	}
+	protected:
+		Soldier currentSoldierData;
+		Soldier baseSoldierData;
 
-	Soldier(std::string _name, Attributes _attributes, ItemInterface item, int _maxItems)
+	public:
+
+		SoldierInstance(int baseSoldierIndex)
+		{
+			baseSoldierData = soldiers[baseSoldierIndex];
+		}
+
+		const Soldier* getCurrentSoldierData() { return &currentSoldierData; }
+		const Soldier* getBaseSoldierData() { return &baseSoldierData; }
+
+
+	};
+
+
+	// used to represent base static soldier data for given soldierTypes
+	class Soldier
 	{
-		name = _name;
-		attributes = _attributes;
-		items.push_back(item);
-		maxItems = _maxItems;
-	}
-};
+		std::string name = "";
+		Attributes attributes;
+		std::vector<ItemInterface> items;
+		int maxItems = 1;
+		// weapons
+		// cost
+
+		bool hasSet = false;
+
+	public:
+
+		Soldier()
+		{
+			assert(hasSet && "Failed to set all necessary data.");
+		}
+
+		Soldier(std::string _name, Attributes _attributes, int _maxItems)
+		{
+			name = _name;
+			attributes = _attributes;
+			maxItems = _maxItems;
+
+			hasSet = true;
+		}
+
+		Soldier(std::string _name, Attributes _attributes, ItemInterface item, int _maxItems)
+		{
+			name = _name;
+			attributes = _attributes;
+			items.push_back(item);
+			maxItems = _maxItems;
+
+			hasSet = true;
+		}
+	};
 
 
-static const Soldier soldiers[]
-{
-	Soldier("Thug", Attributes(6, 2, 0, 10, -1, 10), 1),
-	Soldier("Apothecary", Attributes(6, 1, 0, 10, 3, 12), HealthPotion(0), 1)
+	static const Soldier soldiers[]
+	{
+		Soldier("Thug", Attributes(6, 2, 0, 10, -1, 10), 1),
+		Soldier("Apothecary", Attributes(6, 1, 0, 10, 3, 12), HealthPotion(0), 1)
+	};
+
 }
