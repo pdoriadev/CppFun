580446abedffb4f3e08a0ab8cd3bab0b4b6f6510
Peter Doria <pdoriadev@gmail.com>
2025-03-06 00:49:52 -0800

    Frostgrave updates. Pivoted to json format for spells. Will be able to easily edit spells, let the player be able to create new ones at runtime? Added json parser single include library.

---
+++ CppFun/ForstgraveBuildsBreakdown/SpellSystem.h
@@ -0,0 +1,172 @@
+#pragma once
+#include <string>
+#include <vector>
+#include <assert.h>
+
+/*
+TO-DO:
+	function that gets school info.
+	function thtat gets category info. 
+
+*/
+
+
+namespace Schools
+{
+	enum type
+	{
+		UNASSIGNED,		// 0
+		CHRONOMANCER,	// 1
+		ELEMENTALIST,	// 2
+		ENCHANTER,		// 3
+		ILLUSIONIST,	// 4
+		NECROMANCER,	// 5
+		SIGILIST,		// 6
+		SOOTHSAYER,		// 7
+		SUMMONER,		// 8
+		THAUMATURGE,	// 9
+		WITCH			// 10
+	};
+
+	static const std::string typeStrings[] =
+	{
+		"UNASSIGNED",	// 0
+		"CHRONOMANCER",	// 1
+		"ELEMENTALIST",	// 2
+		"ENCHANTER",	// 3
+		"ILLUSIONIST",	// 4
+		"NECROMANCER",	// 5
+		"SIGILIST",		// 6
+		"SOOTHSAYER",	// 7
+		"SUMMONER",		// 8
+		"THAUMATURGE",	// 9
+		"WITCH"			// 10 
+	};
+
+	static const std::string descriptions[] =
+	{
+		"N/A",															// 0
+		"Time Wizard. Gamblers and risk-takers.",						// 1
+		"Avatar-y. Control of four elements. Damage dealer.",			// 2
+		"Artifer-y.",													// 3
+		"Conjuerer. Trickster.",										// 4
+		"Scribe. Power from runes.",									// 5
+		"Seer. Sage of History. Fortune Teller.",						// 6
+		"Summoner. Summons demons (any creature from another plane)",	// 7
+		"Healer. Support",												// 8
+		"Alchemist. Goth friend of forest."								// 9
+	};
+}
+
+class School
+{
+	Schools::type type = Schools::type::UNASSIGNED;
+	Schools::type alignedTypes[3] =
+	{
+		Schools::type::UNASSIGNED,
+		Schools::type::UNASSIGNED,
+		Schools::type::UNASSIGNED
+	};
+	Schools::type neutralTypes[3] =
+	{
+		Schools::type::UNASSIGNED,
+		Schools::type::UNASSIGNED,
+		Schools::type::UNASSIGNED
+	};
+	Schools::type opposedType = Schools::type::UNASSIGNED;
+};
+
+namespace SpellCategories
+{
+	enum type
+	{
+		UNASSIGNED,		// 0
+		AREA_EFFECT,	// 1
+		LINE_OF_SIGHT,	// 2
+		OUT_OF_GAME_A,	// 3
+		OUT_OF_GAME_B,	// 4
+		SELF_ONLY,		// 5
+		TOUCH			// 6
+	};
+
+	static const std::string typeStrings[] =
+	{
+		"UNASSIGNED",	// 0
+		"AREA_EFFECT",	// 1
+		"LINE_OF_SIGHT",// 2
+		"OUT_OF_GAME_A",// 3
+		"OUT_OF_GAME_B",// 4
+		"SELF_ONLY",	// 5
+		"TOUCH"			// 6
+	};
+
+	static const std::string descriptions[] =
+	{
+		"N/A",	// 0
+		"Spell affects a given area. May (1) affect area around target point \
+			OR(2) affect whole table. If generates attack make separate attack \
+			roll against each target.",												// 1
+		"Spell can be cast on any target that is within line of sight of \
+			spellcaster. Includes figure casting spell. Max range: 24\".",			// 2
+		"Cannot be cast during game. Can only cast after a game. Wizard and \
+			apprentice may each attempt to cast each Out of Game spell once \
+			after each game. Cannot be empowered. Damage not taken from failing \
+			to cast. No experience earned from casting.",							// 3
+		"Cannot be cast during game. Can only cast before a game. Wizard and \
+			apprentice may each attempt to cast each Out of Game spell once \
+			after each game. Cannot be empowered. Damage not taken from failing \
+			to cast. No experience earned from casting.",							// 4
+		"Can only affect the igure that cast the spell. Cannot be cast on anyone \
+			else.",																	// 5
+		"Target must be within 1\" of spellcaster. Spellcasters may cast this on \
+			themselves."															// 6			
+	};
+}
+
+
+
+class spell
+{
+private:
+	std::string name = "";
+	std::string description = "";
+	int baseCastingNumber = -1;
+	double baseRollChance = -1;
+	Schools::type school = Schools::type::UNASSIGNED;
+	std::vector<SpellCategories::type> categories = {SpellCategories::type::UNASSIGNED};
+	bool wizardOnly = false;
+
+public:
+	spell(std::string _name, std::string _desc, Schools::type _school, 
+		int _roll, std::vector<SpellCategories::type> _categories,  bool _wizardOnly)
+	{
+		assert(_name != "" && "name string is empty");
+		name = _name;
+
+		assert(_desc != "" && "description string is empty");
+		description = _desc;
+
+		assert(_school != Schools::type::UNASSIGNED && "School is unassigned");
+		school = _school;
+
+		assert(_roll > 0 && _roll < 21 && "invalid roll value");
+		baseCastingNumber = _roll;
+		baseRollChance = (20 - static_cast<double>(baseCastingNumber)) / 20;
+
+		assert(_categories[0] != SpellCategories::type::UNASSIGNED && "Category is unassigned");
+		for (int i = 0; i < _categories.size(); i++)
+		{
+			categories.push_back(_categories[i]);
+		}
+
+		wizardOnly = _wizardOnly;
+	}
+
+	std::string getName() { return name; }
+	std::string getDescription() { return description; }
+	int getRoll() { return baseCastingNumber; }
+	Schools::type getSchool() { return school; }
+	double getRollChance() { return baseRollChance; }
+	std::vector<SpellCategories::type> * getCategories() { return &categories; }
+};
+
--- CppFun/ForstgraveBuildsBreakdown/spells.h
+++ CppFun/ForstgraveBuildsBreakdown/spells.h
@@ -1,176 +1,11 @@
 #pragma once
-#include <string>
-#include <vector>
-#include <assert.h>
+#include "SpellSystem.h"
 
-/*
-TO-DO:
-	function that gets school info.
-	function thtat gets category info. 
-
-*/
-
-
-namespace Schools
+static const spell spells[]
 {
-	enum type
-	{
-		UNASSIGNED,		// 0
-		CHRONOMANCER,	// 1
-		ELEMENTALIST,	// 2
-		ENCHANTER,		// 3
-		ILLUSIONIST,	// 4
-		NECROMANCER,	// 5
-		SIGILIST,		// 6
-		SOOTHSAYER,		// 7
-		SUMMONER,		// 8
-		THAUMATURGE,	// 9
-		WITCH			// 10
-	};
-
-	static const std::string typeStrings[] =
-	{
-		"UNASSIGNED",	// 0
-		"CHRONOMANCER",	// 1
-		"ELEMENTALIST",	// 2
-		"ENCHANTER",	// 3
-		"ILLUSIONIST",	// 4
-		"NECROMANCER",	// 5
-		"SIGILIST",		// 6
-		"SOOTHSAYER",	// 7
-		"SUMMONER",		// 8
-		"THAUMATURGE",	// 9
-		"WITCH"			// 10 
-	};
-
-	static const std::string descriptions[] =
-	{
-		"N/A",															// 0
-		"Time Wizard. Gamblers and risk-takers.",						// 1
-		"Avatar-y. Control of four elements. Damage dealer.",			// 2
-		"Artifer-y.",													// 3
-		"Conjuerer. Trickster.",										// 4
-		"Scribe. Power from runes.",									// 5
-		"Seer. Sage of History. Fortune Teller.",						// 6
-		"Summoner. Summons demons (any creature from another plane)",	// 7
-		"Healer. Support",												// 8
-		"Alchemist. Goth friend of forest."								// 9
-	};
-}
-
-class School
-{
-	Schools::type type = Schools::type::UNASSIGNED;
-	Schools::type alignedTypes[3] =
-	{
-		Schools::type::UNASSIGNED,
-		Schools::type::UNASSIGNED,
-		Schools::type::UNASSIGNED
-	};
-	Schools::type neutralTypes[3] =
-	{
-		Schools::type::UNASSIGNED,
-		Schools::type::UNASSIGNED,
-		Schools::type::UNASSIGNED
-	};
-	Schools::type opposedType = Schools::type::UNASSIGNED;
-};
-
-namespace SpellCategories
-{
-	enum type
-	{
-		UNASSIGNED,		// 0
-		AREA_EFFECT,	// 1
-		LINE_OF_SIGHT,	// 2
-		OUT_OF_GAME_A,	// 3
-		OUT_OF_GAME_B,	// 4
-		SELF_ONLY,		// 5
-		TOUCH			// 6
-	};
-
-	static const std::string typeStrings[] =
-	{
-		"UNASSIGNED",	// 0
-		"AREA_EFFECT",	// 1
-		"LINE_OF_SIGHT",// 2
-		"OUT_OF_GAME_A",// 3
-		"OUT_OF_GAME_B",// 4
-		"SELF_ONLY",	// 5
-		"TOUCH"			// 6
-	};
-
-	static const std::string descriptions[] =
-	{
-		"N/A",	// 0
-		"Spell affects a given area. May (1) affect area around target point \
-			OR(2) affect whole table. If generates attack make separate attack \
-			roll against each target.",												// 1
-		"Spell can be cast on any target that is within line of sight of \
-			spellcaster. Includes figure casting spell. Max range: 24\".",			// 2
-		"Cannot be cast during game. Can only cast after a game. Wizard and \
-			apprentice may each attempt to cast each Out of Game spell once \
-			after each game. Cannot be empowered. Damage not taken from failing \
-			to cast. No experience earned from casting.",							// 3
-		"Cannot be cast during game. Can only cast before a game. Wizard and \
-			apprentice may each attempt to cast each Out of Game spell once \
-			after each game. Cannot be empowered. Damage not taken from failing \
-			to cast. No experience earned from casting.",							// 4
-		"Can only affect the igure that cast the spell. Cannot be cast on anyone \
-			else.",																	// 5
-		"Target must be within 1\" of spellcaster. Spellcasters may cast this on \
-			themselves."															// 6			
-	};
-}
-
-
-
-class spell
-{
-private:
-	std::string name = "";
-	std::string description = "";
-	int baseCastingNumber = -1;
-	double baseRollChance = -1;
-	SpellCategories::type category = SpellCategories::type::UNASSIGNED;
-	Schools::type school = Schools::type::UNASSIGNED;
-	bool wizardOnly = false;
-
-public:
-	spell(std::string _name, std::string _desc, Schools::type _school, 
-		int _roll, SpellCategories::type _category,  bool _wizardOnly)
-	{
-		assert(_name != "" && "name string is empty");
-		name = _name;
-
-		assert(_desc != "" && "description string is empty");
-		description = _desc;
-
-		assert(_school != Schools::type::UNASSIGNED && "School is unassigned");
-		school = _school;
-
-		assert(_roll > 0 && _roll < 21 && "invalid roll value");
-		baseCastingNumber = _roll;
-		baseRollChance = (20 - static_cast<double>(baseCastingNumber)) / 20;
-
-		assert(_category != SpellCategories::type::UNASSIGNED && "Category is unassigned");
-		category = _category;
-
-		wizardOnly = _wizardOnly;
-	}
-
-	std::string getName() { return name; }
-	std::string getDescription() { return description; }
-	int getRoll() { return baseCastingNumber; }
-	Schools::type getSchool() { return school; }
-	double getRollChance() { return baseRollChance; }
-	SpellCategories::type getCategory() { return category; }
-};
-
-static const spell spells []
-{
-	spell(
-		"ABSORB KNOWLEDGE",
+	spell
+	(
+		"Absorb Knowledge",
 		"Wizard only. This spell allows a wizard to absorb the knowledge from a written		\
 		 without having to read it.A wizard immediately gains 40 experience points			\
 		for casting this spell to represent the speed with which they can gain knowledge.	\
@@ -179,10 +14,12 @@ static const spell spells []
 		reduced to 0 Health.",
 		Schools::type::SIGILIST,
 		12,
-		SpellCategories::type::OUT_OF_GAME_A,
-		true),
-	spell(
-		"ANIMAL COMPANION",
+		{SpellCategories::type::OUT_OF_GAME_A},
+		true
+	),
+	spell
+	(
+		"Animal Companion",
 		"The spellcaster summons an animal companion of their choice from the				\
 		following options to become a permanent member of their warband : bear				\
 		(page 179), ice toad(page 180), snow leopard(page 182), or wolf(page 182).			\
@@ -191,11 +28,12 @@ static const spell spells []
 		+ 3 Will.A spellcaster may only have one animal companion at any time.",
 		Schools::type::WITCH,
 		10,
-		SpellCategories::type::OUT_OF_GAME_B,
+		{ SpellCategories::type::OUT_OF_GAME_B },
 		false
-		),
-	spell(
-		"ANIMATE CONSTRUCT",
+	),
+	spell
+	(
+		"Animate Construct",
 		"It is assumed that the spellcaster has built a construct prior to using this spell to	\
 		animate it.If the spell is successfully cast, the construct immediately becomes a		\
 		permanent member of the warband, taking the place of a soldier.A spellcaster			\
@@ -207,11 +45,12 @@ static const spell spells []
 		specialist soldiers, the others as standard soldiers.",
 		Schools::type::ENCHANTER,
 		10,
-		SpellCategories::type::OUT_OF_GAME_B,
+		{ SpellCategories::type::OUT_OF_GAME_B },
 		false
-		),
-	spell(
-		"ANIMATE SKULL",
+	),
+	spell
+	(
+		"Animate Skull",
 		"The spellcaster fills a skull with magic malice and throws it at an opponent. Place	\
 		one animated skull(page 190) within 6” of the spellcaster.It can be placed				\
 		directly into combat.This skull is an uncontrolled creature.The spellcaster may			\
@@ -220,34 +59,387 @@ static const spell spells []
 		immediately removed from the table.",
 		Schools::type::NECROMANCER,
 		8,
-		SpellCategories::LINE_OF_SIGHT,
+		{ SpellCategories::type::LINE_OF_SIGHT },
 		false
-		),
-	spell(
-		"AWARENESS",
+	),
+	spell
+	(
+		"Awareness",
 		"If this spellcaster is on the table, its warband may add +2 to its Initiative Rolls for	\
 		the purposes of determining the primary player only.This bonus stacks so, if both			\
 		the wizardand the apprentice have cast this spelland are both on the table, the				\
-		player may add + 4 to their Initiative Rolls.The maximum possible bonus is + 4.				\
+		player may add +4 to their Initiative Rolls.The maximum possible bonus is +4.				\
 		This spell counts as active on the spellcaster during the game and may be					\
 		cancelled by anything that cancels spells.",
 		Schools::type::SOOTHSAYER,
 		12,
-		SpellCategories::OUT_OF_GAME_B,
+		{ SpellCategories::type::OUT_OF_GAME_B },
 		false
-		),
-	spell(
-		"BANISH",
+	),
+	spell
+	(
+		"Banish",
 		"All demons within line of sight of the spellcaster must pass an immediate Will			\
 		Roll with a Target Number equal to the Casting Roll.If a demon fails the roll and		\
-		its current Will is + 4 or less, it is immediately reduced to 0 Health and removed		\
-		from the table.If its current Will is + 5 or higher, it suffers damage equal to three	\
+		its current Will is +4 or less, it is immediately reduced to 0 Health and removed		\
+		from the table.If its current Will is +5 or higher, it suffers damage equal to three	\
 		times the amount by which it failed the Will Roll.",
 		Schools::type::THAUMATURGE,
 		10,
-		SpellCategories::LINE_OF_SIGHT,
+		{ SpellCategories::type::LINE_OF_SIGHT },
 		false
-		),
+	),
+	spell
+	(
+		"Beauty",
+		"",
+		Schools::type::ILLUSIONIST,
+		10,
+		{ SpellCategories::type::SELF_ONLY },
+		false
+	),
+	spell
+	(
+		"Blinding Light",
+		"",
+		Schools::type::THAUMATURGE,
+		8,
+		{ SpellCategories::type::LINE_OF_SIGHT },
+		false
+	),
+	spell
+	(
+		"Blink",
+		"",
+		Schools::type::ILLUSIONIST,
+		12,
+		{ SpellCategories::type::LINE_OF_SIGHT },
+		false
+	),
+	spell
+	(
+		"Bone Dart",
+		"",
+		Schools::type::NECROMANCER,
+		10,
+		{ SpellCategories::type::LINE_OF_SIGHT },
+		false
+	),
+	spell
+	(
+		"Bones of the Earth",
+		"",
+		Schools::type::NECROMANCER,
+		10,
+		{ SpellCategories::type::LINE_OF_SIGHT },
+		false
+	),
+	spell
+	(
+		"Brew Potion",
+		"",
+		Schools::type::WITCH,
+		12,
+		{ SpellCategories::type::OUT_OF_GAME_B },
+		false
+	),
+	spell
+	(
+		"Bridge",
+		"",
+		Schools::SIGILIST,
+		10,
+		{ SpellCategories::type::LINE_OF_SIGHT },
+		false
+	),
+	spell
+	(
+		"Call Storm",
+		"",
+		Schools::ELEMENTALIST,
+		12,
+		{ SpellCategories::type::AREA_EFFECT },
+		false
+	),
+	spell
+	(
+		"Circle of Protection",
+		"",
+		Schools::THAUMATURGE,
+		12,
+		{ SpellCategories::type::TOUCH },
+		false
+	),
+	spell
+	(
+		"Combat Awareness",
+		"",
+		Schools::SOOTHSAYER,
+		12,
+		{ SpellCategories::type::TOUCH },
+		false
+	),
+	spell
+	(
+		"Control Animal",
+		"",
+		Schools::WITCH,
+		12,
+		{ SpellCategories::type::LINE_OF_SIGHT },
+		false
+	),
+	spell
+	(
+		"Control Construct",
+		"",
+		Schools::ENCHANTER,
+		10,
+		{ SpellCategories::type::LINE_OF_SIGHT },
+		false
+	),
+	spell
+	(
+		"Control Demon",
+		"",
+		Schools::SUMMONER,
+		10,
+		{ SpellCategories::type::LINE_OF_SIGHT },
+		false
+	),
+	spell
+	(
+		"Control Undead",
+		"",
+		Schools::NECROMANCER,
+		12,
+		{ SpellCategories::type::LINE_OF_SIGHT },
+		false
+	),
+	spell
+	(
+		"Crumble",
+		"",
+		Schools::CHRONOMANCER,
+		10,
+		{ SpellCategories::type::LINE_OF_SIGHT },
+		false
+	),
+	spell
+	(
+		"Curse",
+		"",
+		Schools::WITCH,
+		8,
+		{ SpellCategories::type::LINE_OF_SIGHT },
+		false
+	),
+	spell
+	(
+		"Decay",
+		"",
+		Schools::CHRONOMANCER,
+		12,
+		{ SpellCategories::type::LINE_OF_SIGHT },
+		false
+	),
+	spell
+	(
+		"Destroy Undead",
+		"",
+		Schools::THAUMATURGE,
+		10,
+		{ SpellCategories::type::LINE_OF_SIGHT },
+		false
+	),
+	spell
+	(
+		"Destructive Sphere",
+		"",
+		Schools::ELEMENTALIST,
+		12,
+		{ SpellCategories::type::AREA_EFFECT },
+		false
+	),
+	spell
+	(
+		"Dispel",
+		"",
+		Schools::THAUMATURGE,
+		12,
+		{ SpellCategories::type::LINE_OF_SIGHT },
+		false
+	),
+	spell
+	(
+		"Draining Word",
+		"",
+		Schools::SIGILIST,
+		14,
+		{ SpellCategories::type::AREA_EFFECT },
+		false
+	),
+	spell
+	(
+		"Elemental Ball",
+		"",
+		Schools::ELEMENTALIST,
+		12,
+		{ SpellCategories::type::LINE_OF_SIGHT },
+		false
+	),
+	spell
+	(
+		"Elemental Bolt",
+		"",
+		Schools::ELEMENTALIST,
+		12,
+		{ SpellCategories::type::LINE_OF_SIGHT },
+		false
+	),
+	spell
+	(
+		"Elemental Hammer",
+		"",
+		Schools::ELEMENTALIST,
+		10,
+		{ SpellCategories::type::LINE_OF_SIGHT },
+		false
+	),
+	spell
+	(
+		"Elemental Shield",
+		"",
+		Schools::ELEMENTALIST,
+		10,
+		{ SpellCategories::type::SELF_ONLY },
+		false
+	),
+	spell
+	(
+		"Embed Enchantment",
+		"",
+		Schools::ENCHANTER,
+		14,
+		{ SpellCategories::type::OUT_OF_GAME_A },
+		false
+	),
+	spell
+	(
+		"Enchant Armour",
+		"",
+		Schools::ENCHANTER,
+		8,
+		{ SpellCategories::type::LINE_OF_SIGHT },
+		false
+	),
+	spell
+	(
+		"Explosive Rune",
+		"",
+		Schools::SIGILIST,
+		10,
+		{ SpellCategories::type::LINE_OF_SIGHT },
+		false
+	),
+	spell
+	(
+		"Control Undead",
+		"",
+		Schools::WITCH,
+		12,
+		{ SpellCategories::type::LINE_OF_SIGHT },
+		false
+	),
+	spell
+	(
+		"Familiar",
+		"",
+		Schools::WITCH,
+		10,
+		{ SpellCategories::type::OUT_OF_GAME_B },
+		false
+	),
+	spell
+	(
+		"Fast Act",
+		"",
+		Schools::CHRONOMANCER,
+		8,
+		{ SpellCategories::type::LINE_OF_SIGHT },
+		false
+	),
+	spell
+	(
+		"Fleet Feet",
+		"",
+		Schools::CHRONOMANCER,
+		10,
+		{ SpellCategories::type::LINE_OF_SIGHT },
+		false
+	),
+	spell
+	(
+		"Fog",
+		"",
+		Schools::WITCH,
+		8,
+		{ SpellCategories::type::LINE_OF_SIGHT },
+		false
+	),
+	spell
+	(
+		"Fool's Gold",
+		"",
+		Schools::ILLUSIONIST,
+		10,
+		{ SpellCategories::type::LINE_OF_SIGHT },
+		false
+	),
+	spell
+	(
+		"Furious Quill",
+		"",
+		Schools::SIGILIST,
+		10,
+		{ SpellCategories::type::LINE_OF_SIGHT },
+		false
+	),
+	spell
+	(
+		"Glow",
+		"",
+		Schools::ILLUSIONIST,
+		10,
+		{ SpellCategories::type::LINE_OF_SIGHT },
+		false
+	),
+	spell
+	(
+		"Grenade",
+		"",
+		Schools::ENCHANTER,
+		10,
+		{ SpellCategories::type::LINE_OF_SIGHT },
+		false
+	),
+	spell
+	(
+		"Heal",
+		"",
+		Schools::THAUMATURGE,
+		8,
+		{ SpellCategories::type::LINE_OF_SIGHT },
+		false
+	),
+	spell
+	(
+		"Illusionary Soldier",
+		"",
+		Schools::ILLUSIONIST,
+		12,
+		{ SpellCategories::type::LINE_OF_SIGHT },
+		false
+	),
 	spell(
 		"ILLUSIONIST",
 		"This spell causes anyone who looks on the spellcaster to see a paragon of beauty.		\
@@ -261,8 +453,9 @@ static const spell spells []
 		Six : Bestiary, page 176) or war hounds.",
 		Schools::type::ILLUSIONIST,
 		10,
-		SpellCategories::type::SELF_ONLY,
+		{ SpellCategories::type::SELF_ONLY },
 		false
-		)
+		),
+
 
 };
