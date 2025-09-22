b9f312590ef74e3b26020bd773c61320a99b1dfe
Peter <pdoriadev@gmail.com>
2025-09-22 14:17:49 -0700

    Cryptography - character shift first pass.

---
+++ CppFun/Cryptography/Project1/main.cpp
@@ -0,0 +1,140 @@
+///////////////////////////////////////////////////
+// By pdoria
+///////////////////////////////////////////////////
+
+#include <iostream>
+#include <iomanip>
+#include <vector>
+#include <set>
+#include <cctype>
+
+using namespace std;
+
+enum WaitType 
+{
+	INPUT_ANY,		// = 0
+	INPUT_CONFIRM,	// = 1
+	NO_INPUT_TIMED	// = 2
+};
+
+set<string> validWords = {"a", "and", "at", "as", "be", "but", "do", "for", "have", "he", "i", "in", "is", "it", 
+						"not", "of", "on", "she", "that", "the", "to", "with", "you"};
+
+void decodeLetters(const string& cipher, vector<string>& plainTextOut)
+{
+	for (int i = 0; i < plainTextOut.size(); i++)
+	{
+		plainTextOut[i].resize(cipher.size(), '*');
+		int letterOffset = i + 1;
+		for (int j = 0; j < cipher.size(); j++)
+		{
+
+			// Is this a letter?
+			int valueA = cipher[j] >= 65 && cipher[j] <= 90 ? 65 : 0;
+			if (valueA == 0)
+			{
+				valueA = cipher[j] >= 97 && cipher[j] <= 122 ? 97 : 0;
+			} 
+
+			if (valueA == 0)
+			{
+ 				plainTextOut[i][j] = cipher[j];
+				continue;
+			}
+
+			// Transform cipher to plainText value
+			char plainTextLetter;
+			if (cipher[j] + letterOffset > valueA + 25)
+			{
+				// wrap around to earlier letters
+				plainTextLetter = cipher[j] + letterOffset - valueA + 25 - 1;
+			}
+			else
+			{
+				plainTextLetter = cipher[j] + letterOffset;
+			}
+
+			plainTextOut[i][j] = plainTextLetter;
+		}
+	}
+	
+}
+
+void waitForInput(string messageIn = "\nWaiting For Input. . .", string* messageOut = NULL)
+{
+	cout << messageIn;
+	cin >> *messageOut;
+}
+
+/// <summary>
+/// returns decoded messages that may be valid.
+/// </summary>
+void findValidMessages(const vector<string>& plainTextIn, vector<int>& validIndicesOut)
+{
+	for (int i = 0; i < plainTextIn.size(); i++)
+	{
+		string word = "";
+		for (int j = 0; j < plainTextIn[i].size(); j++)
+		{
+			bool isLetter = (plainTextIn[i][j] >= 65 && plainTextIn[i][j] <= 90) ||
+								(plainTextIn[i][j] >= 97 && plainTextIn[i][j] <= 122);
+			if (isLetter == false)
+			{
+				if (validWords.count(word) > 0)
+				{
+					validIndicesOut.push_back(i);
+					break;
+				}
+
+				word = "";
+				continue;
+			}
+
+			word += tolower(plainTextIn[i][j]);
+		}
+	}
+}
+
+void printValidMessages(const vector<string>& plainTextIn, const vector<int> validIndicesIn)
+{
+	string messageIn = "";
+	for (int i = 0; i < validIndicesIn.size(); i++)
+	{
+		cout << "\nVariation " << validIndicesIn[i];
+		waitForInput("\nInput anything to output next message. \nInput 'y' if you want to conclude this decoding session.", &messageIn);
+	}
+
+	waitForInput("\nEnd of Valid Messages");
+}
+
+
+int main()
+{
+	string message1 = "Mx mw zivc mqtsvxerx jsv epp gsqtyxiv wgmirxmwxw xs fi orsapihkefpi efsyx gcfivwigyvmxc tvmrmgtpiw erh tvegxmgiw.Xlmw mw csyv sttsvxyrmxc!!";
+	string message2 = "zorxv dzh yvtrmmrmt gl tvg evib grivw lu hrggrmt yb svi hrhgvi lm gsv yzmp zmw lu szermt mlgsrmt gl wl lmxv li gdrxv hsv szw kvvkvw rmgl gsv \
+		\n yllp svi hrhgvi dzh ivzwrmt yfg rg szw ml krxgfivh li xlmevihzgrlmh rm rg zmw dszg rh gsv fhv lu z yllp gslftsg zorxv drgslfg krxgfivh \
+		\n li xlmevihzgrlm hl hsv dzh xlmhrwvirmt rm svi ldm nrmw zh dvoo zh hsv xlfow uli gsv slg wzb nzwv svi uvvo evib hovvkb zmw hgfkrw dsvgsvi \
+		\n gsv kovzhfiv lu nzprmt z wzrhb xszrm dlfow yv dligs gsv gilfyov lu tvggrmt fk zmw krxprmt gsv wzrhrvh dsvm hfwwvmob z dsrgv Izyyrg drgs krmp vbvh izm xolhv yb svi";
+
+	vector<string> plainTextVariants = {""};
+	plainTextVariants.resize(26, "");
+	vector<int> validMessages = {};
+	
+	cout << '\n' << setfill('*') << setw(30) << endl;
+	decodeLetters(message1, plainTextVariants);
+	findValidMessages(plainTextVariants, validMessages);
+	printValidMessages(plainTextVariants, validMessages);
+	
+	// reset plainTextValues
+	for (int i = 0; i < plainTextVariants.size(); i++)
+	{
+		plainTextVariants[i] = "";
+	}
+
+	cout << '\n' << setfill('*') << setw(30) << endl;
+	decodeLetters(message2, plainTextVariants);
+	findValidMessages(plainTextVariants, validMessages);
+	printValidMessages(plainTextVariants, validMessages);
+
+	return 0;
+}
