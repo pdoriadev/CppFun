de5bded7a015b67660b40ff0fffba527728783e8
pdoriadev <pdoriadev@gmail.com>
2026-02-11 16:01:10 -0800

    Algo - Python Bash testing and pseudocode.

--- CppFun/AlgoEngineering/pythonBash/pythonBash.py
+++ CppFun/AlgoEngineering/pythonBash/pythonBash.py
@@ -1,115 +1,122 @@
 #!/peter/bin/env python3
 
-# algorithm inputs:
-	# Always start with a light disk on the left
-	# Always of length 2n, where n is the number of light or dark disks. 
-generalCase1 = [0, 1, 0, 1, 0, 1, 0, 1]
-generalCase2 = [0, 1, 0, 1]
-generalCase3 = [0, 1, 0, 1 , 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1]
-edgeCase1 = []
-edgeCase2 = [0, 1]
+generalCase = [0, 1, 0, 1, 0, 1, 0, 1]
+edgeCase1 = [1, 0, 1, 0]
+edgeCase2 = [0]
+edgeCase3 = [0, 1]
+edgeCase4 = [1, 0]
+edgeCase5 = [0, 1, 1, 1, 0, 0]
+edgeCase6 = [1, 1, 1, 0, 0, 0]
+edgeCase7 = [0, 1, 1, 1, 1, 0, 0, 0]
+edgeCase8 = [1, 1, 1, 1, 0, 0, 0, 0]
 
-# print each value of list on a new line
-for i in generalCase1 :
-	print(generalCase1[i])
+def listPrintTests() :
+	# print each value of list on a new line
+	for i in generalCase :
+		print(generalCase[i])
 
-# print list
-print ("\n")
-print(list[0:len(generalCase1)])
+	# print list
+	print ("\n")
+	print (list[0:len(generalCase)])
 
-
-# Print formatted string
-print (f"\nn of generalCase1 list = {len(generalCase1)/2}")
+	# Print formatted string
+	print (f"\nn of generalCase list = {len(generalCase)/2}")
 
 # uses type hint for integer list
-def addList(generalCase1:[int]):
+def addList(generalCase:[int]):
 	sum = 0
 	for i in list:
-		sum += generalCase1[i]
+		sum += generalCase[i]
 
-def lawnMowerSort(diskList:[int], n:int) :
-	print(f"List before sort: {diskList[0:len(diskList)]}")
-	if len(diskList) <= 2 :
-		print("List is already sorted.")
+def lawnMowerSort(unsortedList:[int]) :
+	print(f"List before sort: {unsortedList[0:len(unsortedList)]}")
+	n = int(len(unsortedList) / 2) + len(unsortedList)%2
+	if n <= 1 :
+		print("List is already sorted.", flush=True)
 		return
 
-	# starting mow direction is positive
 	mowDirection:int = 1
-	index:int = 1
-	mows:int = 0
+	index:int = 0
+	runs:int = 0
 	swaps:int = 0
-
-	while mows < n/2 :
+	while runs < n :
 		# Swap if values differ
 			# swappability depends on value. We want 0's on the left and 1's on the right.
 			# 1's get moved right when mowing right. 0's get moved left when mowing left. 
 			# mowDirection switches between 1 and -1. 
-		if diskList[index]*mowDirection > diskList[index+mowDirection]*mowDirection :
-			swap:int = diskList[index+mowDirection]
-			diskList[index+mowDirection] = diskList[index]
-			diskList[index] = swap
-
+		if unsortedList[index]*mowDirection > unsortedList[index+mowDirection]*mowDirection :
+			swap:int = unsortedList[index+mowDirection]
+			unsortedList[index+mowDirection] = unsortedList[index]
+			unsortedList[index] = swap
 			swaps += 1
-
-		# print(f"sort {index}: {diskList[0:len(diskList)]}")
+		print(f"index = {index}, {unsortedList[0:len(unsortedList)]}")
 		# Increment index in current direction we are "mowing".
 		index += mowDirection
-		# Bounds check to change direction
-		if index+mowDirection <= 0 or index+mowDirection >= len(diskList)-1 :
+		# Bounds check to change mow direction
+		if index+mowDirection < 0 or index+mowDirection >= len(unsortedList) :
 			mowDirection *= -1
-			mows += 0.5
-	print(f"List after sort: {diskList[0:len(diskList)]}")
-	print(f"Mows: {mows}\nswaps: {swaps}")
+			runs += 1
 
-	return swaps
-# Starts with leftmost disk. Goes to the right every 2 indexes.
-# When it reaches the end starts at the first or second-most left index.
-# Checks adjacent disks for swaps (i.e. k, k+1) 
-# Total of n runs
-def alternateSort(diskList:[int], n:int):
-	print(f"List before sort: {diskList[0:len(diskList)]}")
-	print(f"n = {n}")
-	if len(diskList) <= 2 :
-		print("List is already sorted.")
+	print(f"List after sort: {unsortedList[0:len(unsortedList)]}")
+	print(f"Mows: {runs/2}\nswaps: {swaps}", flush=True)
+
+def alternateLawnMowerSort(unsortedList:[int]):
+	print(f"List before sort: {unsortedList[0:len(unsortedList)]}")
+	n = len(unsortedList) / 2
+	if n < 1 :
+		print("List is already sorted.", flush=True)
 		return
 
 	startingIndex:int = 0
 	index:int = 0
-	runs:int = 0
 	swaps:int = 0
-	while runs < n:
-		# compare values. If left is greater, then swap.
-		if diskList[index] > diskList[index+1]:
-			temp:int = diskList[index]
-			diskList[index] = diskList[index+1]
-			diskList[index+1] = temp
-			swaps +=1
+	runs:int = 0
+	while runs < n :
+		# Swap up
+		if unsortedList[index] > unsortedList[index+1] :
+			swap:int = unsortedList[index+1]
+			unsortedList[index+1] = unsortedList[index]
+			unsortedList[index] = swap
+			swaps += 1
+		# print(f"index = {index},: {unsortedList[0:len(unsortedList)]}")
 
-		print(f"Index {index}: {diskList[0:len(diskList)]}")
 		index += 2
-
-		# Bounds check. Never going to swap when index is equal to the second to last index. 
-		if index >= len(diskList)-2 :
+		# Bounds check
+		if len(unsortedList) == 2:
+			break
+		elif index+1 >= len(unsortedList) :
+			# flips starting index between one and zero
 			startingIndex = 1 - startingIndex
 			index = startingIndex
 			runs += 1
-	print(f"List after sort: {diskList[0:len(diskList)]}")
-	print(f"Swaps={swaps}\nRuns={runs}")
-	return swaps
+
+	print(f"List after sort: {unsortedList[0:len(unsortedList)]}")
+	print(f"Runs: {runs}\nswaps: {swaps}")
 
 
 if __name__ == "__main__":
-	lawnMowerSort(generalCase1.copy(), len(generalCase1) / 2)
-	lawnMowerSort(generalCase2.copy(), len(generalCase2) / 2)
-	lawnMowerSort(generalCase3.copy(), len(generalCase3) / 2)
-	lawnMowerSort(edgeCase1.copy(), len(edgeCase1) / 2)
-	lawnMowerSort(edgeCase2.copy(), len(edgeCase2) / 2)
+	print("=============================\nLawn Mower Sorts")
+	lawnMowerSort(generalCase.copy())
+	lawnMowerSort(edgeCase1.copy())
+	lawnMowerSort(edgeCase2.copy())
+	lawnMowerSort(edgeCase3.copy())
+	lawnMowerSort(edgeCase4.copy())
+	lawnMowerSort(edgeCase5.copy())
+	lawnMowerSort(edgeCase6.copy())
+	lawnMowerSort(edgeCase7.copy())
+	lawnMowerSort(edgeCase8.copy())
 
-	alternateSort(generalCase1.copy(), len(generalCase1) / 2)
-	alternateSort(generalCase2.copy(), len(generalCase2) / 2)
-	alternateSort(generalCase3.copy(), len(generalCase3) / 2)
-	alternateSort(edgeCase1.copy(), len(edgeCase1) / 2)
-	alternateSort(edgeCase2.copy(), len(edgeCase2) / 2)
+
+	print("=============================\nAlternate Lawn Mower Sorts")
+	alternateLawnMowerSort(generalCase.copy())
+	alternateLawnMowerSort(edgeCase1.copy())
+	alternateLawnMowerSort(edgeCase2.copy())
+	alternateLawnMowerSort(edgeCase3.copy())
+	alternateLawnMowerSort(edgeCase4.copy())
+	alternateLawnMowerSort(edgeCase5.copy())
+	alternateLawnMowerSort(edgeCase6.copy())
+	alternateLawnMowerSort(edgeCase7.copy())
+	alternateLawnMowerSort(edgeCase8.copy())
 
 # class Disk
 
