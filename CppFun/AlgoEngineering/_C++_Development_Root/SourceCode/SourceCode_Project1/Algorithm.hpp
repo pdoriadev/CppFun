2e73d710db9ccaabd79ca64ce1adf9fe03d8cbff
pdoriadev <pdoriadev@gmail.com>
2026-02-16 20:38:43 -0800

    Algo - Cpp implementation of lawnmower and alternate sort. Deleted swp file.

--- CppFun/AlgoEngineering/_C++_Development_Root/SourceCode/SourceCode_Project1/Algorithm.hpp
+++ CppFun/AlgoEngineering/_C++_Development_Root/SourceCode/SourceCode_Project1/Algorithm.hpp
@@ -5,6 +5,8 @@
   /// Hint:  Include what you use, use what you include
   ///
   /// Do not put anything else in this section, i.e. comments, classes, functions, etc.  Only #include directives
+#include <iostream>
+#include <stdint.h>
 #include "Disks.hpp"
 /////////////////////// END-TO-DO (1) ////////////////////////////
 
@@ -38,7 +40,41 @@ struct Algorithm_1 : Algorithm
     ///               where j is the number of traversals completed (e.g., starts with 1), for example
     ///                    1: [⚪, ⚫, ⚪, ⚫]
     ///                    2: [⚪, ⚪, ⚫, ⚫]
+		uint64_t n = disks.size() / 2;
 
+		if (n <= 1)
+		{
+			// Disks is already sorted
+			return 0;
+		}
+
+		uint64_t swaps = 0;
+		uint64_t j = 0; // j/2 = completedMows
+		uint8_t mowDirection = 1;
+		for (uint64_t i = 0; j < n; )
+		{
+			// Swap Check.
+			if (static_cast<uint8_t>(disks[i].color()) * mowDirection > static_cast<uint8_t>(disks[i+mowDirection].color()) * mowDirection)
+			{
+				Disk swap = disks[i].color();
+				disks[i] = disks[i+mowDirection];
+				disks[i+mowDirection] = swap;
+				swaps+=1;
+			}
+
+			i+=mowDirection;
+
+			// Bounds Check. If bounds is exceeded, switch mow direction.
+			if (i+mowDirection <= 0 || i+mowDirection >= 2*n-1)
+			{
+				mowDirection *= (-1);
+				j++;
+				// :>3 means 3 columns, right justified.
+				std::print( std::cout, "{:>3}: {::s}\n", j, disks );
+			}
+		}
+
+		return swaps;
   /////////////////////// END-TO-DO (2) ////////////////////////////
   }
 };
@@ -63,7 +99,40 @@ struct Algorithm_2 : Algorithm
     ///               where j is the number of traversals completed (e.g., starts with 1), for example
     ///                    1: [⚪, ⚫, ⚪, ⚫]
     ///                    2: [⚪, ⚪, ⚫, ⚫]
+		if (n <= 1)
+		{
+			// Disks is already sorted
+			return 0;
+		}
 
+		uint64_t swaps = 0;
+		uint64_t j = 0;
+		uint64_t startingIndex = 0;
+		for (int i = 0; j < n; )
+		{
+			// Swap Check.
+			if (static_cast<uint8_t>(disks[i].color()) > static_cast<uint8_t>(disks[i+1].color()))
+			{
+				Disk swap = disks[i];
+				disks[i] = disks[i+1];
+				disks[i+1] = swap;
+				swaps++;
+			}
+
+			i+=2
+
+			// Bounds Check. If bounds is exceeded, switch the starting index between
+				// 0 and 1, and start the loop again.
+			if (i+1 >= n-1)
+			{
+				startingIndex = 1 - startingIndex;
+				i = startingIndex;
+				j++;
+				std::print( std::cout, "{:>3}: {::s}\n", j, disks );
+			}
+		}
+
+		return swaps;
   /////////////////////// END-TO-DO (3) ////////////////////////////
   }
 };
