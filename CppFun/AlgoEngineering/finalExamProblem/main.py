14adbdbacec71c116c5204c6b3f495e9c5abdc12
Peter D <pdoriadev@gmail.com>
2026-08-27 13:18:56 -0700

    AlgoEngineering changes from last semester. I'm not sure what all this is.

---
+++ CppFun/AlgoEngineering/finalExamProblem/main.py
@@ -0,0 +1,20 @@
+/*The gardener problem:
+  input: a vector of m packs of flower seedlings sold by a nursery, where each pack is a pair (q, c) indicating that if you buy q (positive integer) number of that flower seedlings, you will need c (a positive integer) drippers to water the q seedlings; and a desired number of seedlings n
+  output:   	a vector of packs of flower seedlings, not necessarily distinct, accounting for exactly n seedlings and of minimal total number of drippers
+*/
+
+/*
+Design and write by hand the pseudocode (clear, correct, and terminates) for a dynamic programming algorithm that solves this problem on paper (no electronic writing devices). Scan your work to a PDF file and upload the file to this question. Handwriting must be extremely legible and readable for credit.
+*/
+
+//-// Problem breakdown
+// m packs of seedling pairs (q, c)
+//   q = number of seedlings
+//   c = drippers required for q seedlings 
+// desired number of seedlings n.
+//
+// output  
+int main()
+{
+
+}
